0eaba8af5d5717924adc7073123392e9c2c274fd
Jeremy <jeremyVDLRC@gmail.com>
2021-01-30 17:33:49 +1100

    first function test

---
+++ src/example.test.ts
@@ -0,0 +1,21 @@
+// @flow
+
+import sumPositiveNumbers from './example.ts';
+
+describe('when arguments passed are positive numbers', () => {
+  test('should return the right answer', () => {
+    expect(sumPositiveNumbers(4, 5)).toBe(9);
+  });
+});
+
+describe('when one of the arguments is a negative number', () => {
+  test('should throw an error', () => {
+    let error;
+    try {
+      sumPositiveNumbers(-1, 5);
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeDefined();
+  });
+});
